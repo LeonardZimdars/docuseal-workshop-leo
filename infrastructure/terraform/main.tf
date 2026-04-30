4dfb6166104d47a165d6dfacb4586fb756eede91
Leonard Zimdars <leonard.zimdars@web.de>
2026-04-30 18:07:49 +0200

    add IaC condig

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,1 +1,36 @@
-# TODO: Construct the Terraform script to provision a Hetzner server 
+terraform {
+	cloud {
+	  organization = "leonard-zimdars"  # TODO: change to your own TF Cloud org
+	  workspaces {
+	    name = "leo" # TODO: change your workspace name
+	  }
+	}
+
+	required_providers {
+	  hcloud = {
+	    source  = "hetznercloud/hcloud"
+	    version = "~> 1.57"
+	  }
+	}
+}
+
+provider "hcloud" {
+	token = var.hcloud_token
+}
+
+resource "hcloud_ssh_key" "default" {
+	name       = "workshop-terraform-key-leo-1"
+	public_key = var.ssh_public_key
+}
+
+resource "hcloud_server" "csee_server" {
+	name        = var.name
+	image       = var.os_type
+	server_type = var.server_type
+	location    = var.location
+	ssh_keys    = [hcloud_ssh_key.default.id]
+}
+
+output "csee_server_ip" {
+	value = hcloud_server.csee_server.ipv4_address
+}
